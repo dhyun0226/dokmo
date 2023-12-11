4afe34249520b8c3c181a159235c100ac983e5e9
leedh <dhyun0226@gmail.com>
2023-12-11 06:01:56 +0000

    init

---
+++ src/components/Auth.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="row flex-center flex" @submit.prevent="handleLogin({ email, password })">
+    <div class="col-6 form-widget">
+      <h1 class="header">Supabase + Vue 3</h1>
+      <p class="description">Sign in via magic link with your email below</p>
+      <div>
+        <input class="inputField" required type="email" placeholder="Your email" v-model="email" />
+      </div>
+      <div>
+        <input class="inputField" required type="password" placeholder="Your password" v-model="password" />
+      </div>
+      <div>
+        <input
+          type="submit"
+          class="button block"
+          :value="loading ? 'Loading' : 'Send magic link'"
+          :disabled="loading"
+        />
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { handleLogin } from '@/vuetils/useAuth'
+
+const loading = ref(false)
+const email = ref('')
+const password = ref('')
+
+</script>
