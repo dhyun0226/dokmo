52c7ccf8371d217c4457118dad7f4629a8e89993
LeeDonghyun <donghyun.lee@osstem.com>
2023-12-12 09:25:12 +0900

    수정

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,9 +8,7 @@
         type="text"
         placeholder="What do you need to?"
       />
-      <button @click="insertTask" class="btn-black">
-        Add
-      </button>
+      <button @click="insertTask" class="btn-black">Add</button>
     </div>
 
     <div class="bg-white shadow overflow-hidden rounded-md">
@@ -21,62 +19,49 @@
   </div>
 </template>
 
-<script>
+<script setup>
 /* eslint-disable @typescript-eslint/camelcase */
-import { defineComponent, ref } from 'vue'
-import Todo from '@/components/Todo.vue'
-import { allTodos, fetchTodos, addTodo } from '@/vuetils/useTodo'
-import { userSession } from '@/vuetils/useAuth'
+import { defineComponent, ref } from "vue";
+import Todo from "@/components/Todo.vue";
+import { allTodos, fetchTodos, addTodo } from "@/vuetils/useTodo";
+import { userSession } from "@/vuetils/useAuth";
 
-export default defineComponent({
-  name: 'TodoList',
-  components: {
-    Todo,
-  },
+await fetchTodos();
 
-  async setup() {
-    await fetchTodos()
+const task = ref("");
 
-    const task = ref('')
+/**
+ * Wrapper function adding a new todo for additional client-side error handling.
+ */
+async function insertTask() {
+  // Guard for short task descriptions which will fail db policy.
+  if (task.value.length <= 3) {
+    alert("Please make your task a little more descriptive");
+    return;
+  }
+  // Type check to ensure user is still logged in.
+  if (userSession?.value === null) {
+    alert("Please log in again");
+    return;
+  }
+  try {
+    // Try and write the data to the database.
+    const todo = await addTodo({
+      user_id: userSession.value.user.id,
+      task: task.value,
+    });
 
-    /**
-     * Wrapper function adding a new todo for additional client-side error handling.
-     */
-    async function insertTask() {
-      // Guard for short task descriptions which will fail db policy.
-      if (task.value.length <= 3) {
-        alert('Please make your task a little more descriptive')
-        return
-      }
-      // Type check to ensure user is still logged in.
-      if (userSession?.value === null) {
-        alert('Please log in again')
-        return
-      }
-      try {
-        // Try and write the data to the database.
-        const todo = await addTodo({ user_id: userSession.value.user.id, task: task.value })
-
-        // If there was no response, don't do anything.
-        if (!todo) {
-          return
-        }
-        // Otherwise, push the response into allTodos.
-        allTodos.value.push(todo)
-
-        // Reset input field.
-        task.value = ''
-      } catch (err) {
-        console.error('Unknown error when adding todo', err)
-      }
+    // If there was no response, don't do anything.
+    if (!todo) {
+      return;
     }
+    // Otherwise, push the response into allTodos.
+    allTodos.value.push(todo);
 
-    return {
-      task,
-      allTodos,
-      insertTask,
-      userSession,
-    }
-  },
-})
+    // Reset input field.
+    task.value = "";
+  } catch (err) {
+    console.error("Unknown error when adding todo", err);
+  }
+}
 </script>
